f5788812634887f6bd8b50c18204dc3c201ae3ed
nourbalaha <nourbalaha0@gmail.com>
2018-08-15 17:46:04 +0900

    fix: fixed the interval

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default class App extends Component {
           timer: this.state.timer.subtract(1, "seconds"),
           isWorking: true
         });
-      }, 10);
+      }, 1000);
     }
   };
 
